7c1ac75e68d818ca768ecf417696c1918f2653d7
Atul Ballav Kharel <atulballavk@gmail.com>
2024-03-14 10:53:04 +0545

    Revert "smtp config"
    This reverts commit 19dcd44db927ced9f8e1932539791296af18aa14.

--- pages/_app.js
+++ pages/_app.js
@@ -11,7 +11,7 @@ function MyApp({ Component, pageProps }) {
     setLoading(true);
     setTimeout(() => {
       setLoading(false);
-    }, 10);
+    }, 2000);
 
     if (typeof window !== "undefined") {
       window.WOW = require("wowjs");
